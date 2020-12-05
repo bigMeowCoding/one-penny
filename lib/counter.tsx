390cae1ee32e8631ab71390816f6abe8e1da47f4
zhouyijun <zhou495248579@hotmail.com>
2020-12-05 21:02:31 +0800

    use reducer

--- lib/counter.tsx
+++ lib/counter.tsx
@@ -1,18 +1,55 @@
-import React, { FC, useState } from "react";
+import React, { FC, useReducer } from "react";
+const initState = {
+  n: 0,
+};
+interface State {
+  n: number;
+}
+interface Action {
+  type: string;
+}
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "add":
+      return {
+        ...state,
+        n: state.n + 1,
+      };
+    case "remove":
+      return {
+        ...state,
+        n: state.n - 1,
+      };
+    default:
+      return state;
+  }
+};
 
 const Counter: FC = function () {
-  const [user, setUser] = useState<{ name: string; age: number }>({
-    name: "zyj",
-    age: 10,
-  });
+  const [state, dispatch] = useReducer(reducer, initState);
+  const { n } = state;
+  console.log(n);
   return (
-    <React.Fragment>
-      <div>{user?.name}</div>
-      <button onClick={() => {
-          user.name = 'z';
-          setUser(user)
-      }}>change</button>
-    </React.Fragment>
+    <div>
+      <div>{n}</div>
+      <button
+        onClick={() => {
+          dispatch({
+            type: "add",
+          });
+        }}
+      >
+        add
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "remove" });
+        }}
+      >
+        remove
+      </button>
+    </div>
   );
 };
 
