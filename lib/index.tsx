e886a0ced362007464a80f6e7ca7419306b5967a
zhouyijun <zhou495248579@hotmail.com>
2020-12-10 01:03:43 +0800

    index export

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,14 +1,1 @@
-import React, { Fragment, MouseEventHandler } from "react";
-import ReactDOM from "react-dom";
-import Icon from "./icon/icon";
-
-const clickHandle: MouseEventHandler<SVGElement> = function (e) {
-  console.log(e);
-};
-
-ReactDOM.render(
-  <Fragment>
-    <Icon name="wechat"  onClick={clickHandle} />
-  </Fragment>,
-  document.querySelector("#root")
-);
+export { default as Icon } from "./icon/icon";
