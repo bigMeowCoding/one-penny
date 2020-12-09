6a3286a3f8e0500f9116b33ce4751c8531563ed7
zhouyijun <zhou495248579@hotmail.com>
2020-12-10 00:37:32 +0800

    修改webpack config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,33 +3,6 @@ const HtmlWebpackPlugin = require("html-webpack-plugin"),
 const plugins = config.plugins || [];
 module.exports = Object.assign({}, config, {
   mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: "awesome-typescript-loader",
-        exclude: [
-          /node_modules\/(?!(my_main_package\/what_i_need_to_include)\/).*/,
-          /__test__/,
-        ],
-      },
-      {
-        test: /\.svg$/,
-        loader: "svg-sprite-loader",
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
-      },
-    ],
-  },
   devServer: {
     open: true,
   },
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,33 @@ module.exports = {
   entry: {
     index: "./lib/index.tsx",
   },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader",
+        exclude: [
+          /node_modules\/(?!(my_main_package\/what_i_need_to_include)\/).*/,
+          /__test__/,
+        ],
+      },
+      {
+        test: /\.svg$/,
+        loader: "svg-sprite-loader",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
   output: {
     path: path.resolve(__dirname, "dist/lib"),
     library: "zyjUI",
