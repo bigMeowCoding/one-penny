207bfad1910551d8f9c6a5aba45a999888db1f42
zhouyijun <zhou495248579@hotmail.com>
2020-12-17 00:25:59 +0800

    function refactor makeClassByPrefix

--- lib/common/utils/makeClassByPrefix.ts
+++ lib/common/utils/makeClassByPrefix.ts
@@ -9,21 +9,16 @@ export default function (prefix: string) {
    * @param options 直接添加的className
    */
   return (className: string | ClassToggle, options?: Options) => {
-    let result;
     const namesObject =
       typeof className === "string" ? { [className]: true } : className;
     const names = Object.entries(namesObject)
       .filter((kv) => kv[1])
       .map((item) => item[0]);
-    result = names
+    return names
       .map((name) => {
         return [prefix, name].filter(Boolean).join("-");
       })
+      .concat(options ? options.extra : [])
       .join(" ");
-
-    if (options && options.extra) {
-      result = [result, options.extra].filter(Boolean).join(" ");
-    }
-    return result;
   };
 }
