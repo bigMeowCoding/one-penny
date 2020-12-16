bbcfd9d1989dc7673de9ab207b316424b8bc3579
zhouyijun <zhou495248579@hotmail.com>
2020-12-16 23:38:11 +0800

    修改makeClassByPrefix方法

--- lib/common/utils/makeClassByPrefix.ts
+++ lib/common/utils/makeClassByPrefix.ts
@@ -1,5 +1,30 @@
-export default function (prefix: string): (param?: string) => string {
-  return (className?: string) => {
-    return [prefix, className].filter(Boolean).join("-");
+interface ClassToggle {
+  [K: string]: boolean;
+}
+interface Options {
+  extra: string;
+}
+export default function (prefix: string) {
+  /**
+   * @param options 直接添加的className
+   */
+  return (className?: string | ClassToggle, options?: Options) => {
+    let result;
+    if (typeof className === "string" || typeof className === "undefined") {
+      result = [prefix, className].filter(Boolean).join("-");
+    } else {
+      const names = Object.entries(className)
+        .filter((kv) => kv[1])
+        .map((item) => item[0]);
+      result = names
+        .map((name) => {
+          return [prefix, name].filter(Boolean).join("-");
+        })
+        .join(" ");
+    }
+    if (options && options.extra) {
+      result = [result, options.extra].filter(Boolean).join(" ");
+    }
+    return result;
   };
 }
