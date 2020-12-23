df4e85787bcc321ae52ecc1bdb4b30b2b295817b
zhouyijun <zhou495248579@hotmail.com>
2020-12-23 22:40:20 +0800

    add useWave

---
+++ lib/common/hooks/useWave.ts
@@ -0,0 +1,18 @@
+import { MutableRefObject, useEffect, useRef, useState } from "react";
+import { changeWaveBaseColorOnClick } from "../../button/_util";
+
+const useWave: () => { waveColor: string; ref: MutableRefObject<any> } = () => {
+  const ref = useRef(null),
+    [waveColor, setWaveColor] = useState("");
+
+  useEffect(() => {
+    const btnElement = ref.current;
+    console.log(btnElement);
+    if (btnElement) {
+      changeWaveBaseColorOnClick(btnElement, setWaveColor);
+    }
+  }, []);
+  return { waveColor, ref };
+};
+
+export default useWave;
