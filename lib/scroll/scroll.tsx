7d6f4b243f989f17107ecbff01a79dd6ac7c9d09
zhouyijun <zhou495248579@hotmail.com>
2020-12-28 16:26:35 +0800

    compute scrollbar height

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -1,4 +1,4 @@
-import React, { FC, HTMLAttributes } from "react";
+import React, { FC, HTMLAttributes, useEffect, useRef, useState } from "react";
 import makeClassByPrefix from "../common/utils/makeClassByPrefix";
 import "./scroll.scss";
 import scrollbarWidth from "../common/utils/scroll-bar-width";
@@ -7,13 +7,30 @@ interface Props extends HTMLAttributes<HTMLDivElement> {}
 const addClassByPrefix = makeClassByPrefix("zyj-ui-scroll");
 
 const Scroll: FC<Props> = function ({ className, children, ...rest }) {
+  const [scrollBarHeight, setScrollBarHeight] = useState<number>();
+  const ref = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (!ref.current) {
+      return;
+    }
+    const scrollHeight = ref.current.scrollHeight,
+      clientHeight = ref.current.getBoundingClientRect().height;
+    setScrollBarHeight((clientHeight * clientHeight) / scrollHeight);
+  }, []);
   return (
     <div className={addClassByPrefix("wrapper")} {...rest}>
       <div
+        ref={ref}
         className={addClassByPrefix("inner")}
         style={{ right: -1 * scrollbarWidth() }}
       >
         {children}
+      </div>
+      <div className={addClassByPrefix("bar-wrapper")}>
+        <div
+          className={addClassByPrefix("bar")}
+          style={{ height: scrollBarHeight }}
+        />
       </div>
     </div>
   );
