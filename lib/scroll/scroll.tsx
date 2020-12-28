4facf7fb2dc5567f24c03159f106c13027a23973
zhouyijun <zhou495248579@hotmail.com>
2020-12-28 17:48:31 +0800

    scroll effect

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -1,4 +1,11 @@
-import React, { FC, HTMLAttributes, useEffect, useRef, useState } from "react";
+import React, {
+  FC,
+  HTMLAttributes,
+  UIEventHandler,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import makeClassByPrefix from "../common/utils/makeClassByPrefix";
 import "./scroll.scss";
 import scrollbarWidth from "../common/utils/scroll-bar-width";
@@ -6,9 +13,11 @@ import scrollbarWidth from "../common/utils/scroll-bar-width";
 interface Props extends HTMLAttributes<HTMLDivElement> {}
 const addClassByPrefix = makeClassByPrefix("zyj-ui-scroll");
 
-const Scroll: FC<Props> = function ({ className, children, ...rest }) {
-  const [scrollBarHeight, setScrollBarHeight] = useState<number>();
-  const ref = useRef<HTMLDivElement>(null);
+const Scroll: FC<Props> = ({ className, children, ...rest }) => {
+  const [scrollBarHeight, setScrollBarHeight] = useState<number>(),
+    [scrollBarWidth, setScrollBarWidth] = useState<number>(),
+    ref = useRef<HTMLDivElement>(null),
+    [scrollBarTop, setScrollBarTop] = useState<number>();
   useEffect(() => {
     if (!ref.current) {
       return;
@@ -16,20 +25,35 @@ const Scroll: FC<Props> = function ({ className, children, ...rest }) {
     const scrollHeight = ref.current.scrollHeight,
       clientHeight = ref.current.getBoundingClientRect().height;
     setScrollBarHeight((clientHeight * clientHeight) / scrollHeight);
+    setScrollBarWidth(scrollbarWidth());
   }, []);
+  const onScroll: UIEventHandler = () => {
+    if (!ref.current) {
+      return;
+    }
+    const scrollHeight = ref.current.scrollHeight,
+      scrollTop = ref.current.scrollTop,
+      clientHeight = ref.current.getBoundingClientRect().height;
+    setScrollBarHeight((clientHeight * clientHeight) / scrollHeight);
+    setScrollBarTop((scrollTop * clientHeight) / scrollHeight);
+  };
   return (
     <div className={addClassByPrefix("wrapper")} {...rest}>
       <div
         ref={ref}
+        onScroll={onScroll}
         className={addClassByPrefix("inner")}
-        style={{ right: -1 * scrollbarWidth() }}
+        style={{ right: -1 * (scrollBarWidth || 0) }}
       >
         {children}
       </div>
       <div className={addClassByPrefix("bar-wrapper")}>
         <div
           className={addClassByPrefix("bar")}
-          style={{ height: scrollBarHeight }}
+          style={{
+            height: scrollBarHeight,
+            transform: `translateY(${scrollBarTop}px)`,
+          }}
         />
       </div>
     </div>
