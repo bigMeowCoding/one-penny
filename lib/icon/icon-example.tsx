b26d8fadf6efcce260ec6186ca7de395fca823ad
zhouyijun <zhou495248579@hotmail.com>
2020-12-31 16:26:50 +0800

    pending icon add

--- lib/icon/icon-example.tsx
+++ lib/icon/icon-example.tsx
@@ -2,13 +2,12 @@ import React from "react";
 import { FC } from "react";
 import Icon from "./icon";
 import "./icon-example.scss";
-const IconExample: FC = function () {
-  return (
-    <div>
-      <Icon name="wechat" />
-      <Icon name="cat" />
-      <Icon name="loading" className="loading-icon" />
-    </div>
-  );
-};
+const IconExample: FC = () => (
+  <div>
+    <Icon name="wechat" />
+    <Icon name="cat" />
+    <Icon name="loading" className="loading-icon" />
+    <Icon name="pending" className="loading-icon" />
+  </div>
+);
 export default IconExample;
--- lib/icon/icon.tsx
+++ lib/icon/icon.tsx
@@ -2,15 +2,22 @@ import React, { FC, SVGAttributes } from "react";
 import "./importAllIcons";
 import "./icon.scss";
 import classNames from "../common/utils/classNames";
+import makeClassByPrefix from "../common/utils/makeClassByPrefix";
 interface Props extends SVGAttributes<SVGElement> {
   name: string;
 }
+const addClassByPrefix = makeClassByPrefix("zyj-ui-icon");
 
-const Icon: FC<Props> = function ({ name, className, ...rest }) {
-  return (
-    <svg className={classNames("zyj-ui-icon", className)} {...rest}>
-      <use xlinkHref={`#${name}`} />
-    </svg>
-  );
-};
+const Icon: FC<Props> = ({ name, className, ...rest }) => (
+  <svg
+    className={classNames(
+      addClassByPrefix(""),
+      className,
+      name === "pending" ? addClassByPrefix("loading") : ""
+    )}
+    {...rest}
+  >
+    <use xlinkHref={`#${name}`} />
+  </svg>
+);
 export default Icon;
