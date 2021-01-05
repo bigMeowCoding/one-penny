6b4e8f24a94b0f501190e44ed3db58a09df3f6d1
zhouyijun <zhou495248579@hotmail.com>
2021-01-05 10:44:57 +0800

    remove map key error

--- lib/date-picker/date-picker-example.tsx
+++ lib/date-picker/date-picker-example.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import { FC } from "react";
 import DatePicker from "./date-picker";
 
-
 const DatePickerExample: FC = () => {
   return (
     <article>
       <section>
         <h4>example 1</h4>
         <div>
-          <DatePicker />
+          <DatePicker
+            onChange={(d) => {
+              console.log(d);
+            }}
+          />
         </div>
       </section>
     </article>
--- lib/date-picker/date-picker.tsx
+++ lib/date-picker/date-picker.tsx
@@ -3,11 +3,11 @@ import { FC } from "react";
 import "./date-picker.scss";
 import HoverPanel from "../common/component/hover-panel/hover-panel";
 import Input from "../input/input";
-import dayjs from "dayjs";
+import dayjs, { Dayjs } from "dayjs";
 import makeClassByPrefix, {
   makeComponentPrefixClass,
 } from "../common/utils/makeClassByPrefix";
-
+type OnChangeType = (date: Date) => void;
 const weeks = ["一", "二", "三", "四", "五", "六", "日"];
 const addClassByPrefix = makeClassByPrefix(
   makeComponentPrefixClass("date-picker")
@@ -25,52 +25,52 @@ function computeMonthStartEndDate(dayjsDate: dayjs.Dayjs) {
 
 function addPrevAdditionalDays(
   additionAddDaysCountPrev: number,
-  days: Date[],
+  days: Dayjs[],
   startDate: dayjs.Dayjs
 ) {
   let n = additionAddDaysCountPrev;
   while (n) {
-    days.push(startDate.subtract(n, "day").toDate());
+    days.push(startDate.subtract(n, "day"));
     n--;
   }
 }
 
 function addSuffixAdditionalDays(
   additionAddDaysCountSuffix: number,
-  days: Date[],
+  days: Dayjs[],
   endDate: dayjs.Dayjs
 ) {
   let n = 1;
   while (n <= additionAddDaysCountSuffix) {
-    days.push(endDate.add(n, "day").toDate());
+    days.push(endDate.add(n, "day"));
     n++;
   }
   return n;
 }
 
-function computeAllDays(monthFirstDate: Date, monthLastDate: Date): Date[] {
+function computeAllDays(monthFirstDate: Date, monthLastDate: Date): Dayjs[] {
   const startDate = dayjs(monthFirstDate),
     endDate = dayjs(monthLastDate),
     endDay = endDate.get("date"),
     startDateWeek = startDate.get("day"),
     endDateWeek = endDate.get("day"),
-    days: Date[] = [];
+    days: Dayjs[] = [];
   const additionAddDaysCountPrev = startDateWeek === 0 ? 6 : startDateWeek - 1,
     additionAddDaysCountSuffix = endDateWeek === 0 ? 0 : 7 - endDateWeek;
   addPrevAdditionalDays(additionAddDaysCountPrev, days, startDate);
   addSuffixAdditionalDays(additionAddDaysCountSuffix, days, endDate);
   let n = 0;
   while (n < endDay) {
-    days.push(startDate.add(n, "day").toDate());
+    days.push(startDate.add(n, "day"));
     n++;
   }
   return days;
 }
 
-function convertDaysDate(days: Date[]) {
-  const result: Date[][] = [];
+function convertDaysDate(days: Dayjs[]): Dayjs[][] {
+  const result: Dayjs[][] = [];
   while (days.length) {
-    const temp: Date[] = [];
+    const temp: Dayjs[] = [];
     let n = 0;
     while (n < 7) {
       const day = days.shift();
@@ -86,7 +86,8 @@ function convertDaysDate(days: Date[]) {
 
 const Content: FC<{
   date: Date;
-}> = ({ date }) => {
+  onChange: OnChangeType;
+}> = ({ date, onChange }) => {
   const year = date.getFullYear(),
     dayjsDate = dayjs(date),
     month = dayjsDate.get("month") + 1;
@@ -94,9 +95,8 @@ const Content: FC<{
   const { monthFirstDate, monthLastDate } = computeMonthStartEndDate(dayjsDate);
   // 将本月的日期全部放入数组
   const daysList = computeAllDays(monthFirstDate, monthLastDate);
-  const days = convertDaysDate(daysList);
-  console.log(days);
   // 根据本月日期始末补充开头和结尾日期
+  const days = convertDaysDate(daysList);
   return (
     <div className={addClassByPrefix("days")}>
       <div className={addClassByPrefix("days-header")}>
@@ -117,11 +117,25 @@ const Content: FC<{
             </tr>
           </thead>
           <tbody>
-            {days.map((dayList) => {
+            {days.map((dayList, index) => {
               return (
-                <tr>
-                  {dayList.map((day) => {
-                    return <td>{dayjs(day).get('date')}</td>;
+                <tr key={index}>
+                  {dayList.map((day, dayIndex) => {
+                    return (
+                      <td
+                        key={dayIndex}
+                        onClick={() => {
+                          onChange(day.toDate());
+                        }}
+                        className={
+                          day.get("month") !== dayjsDate.get("month")
+                            ? "other-month"
+                            : ""
+                        }
+                      >
+                        {day.get("date")}
+                      </td>
+                    );
                   })}
                 </tr>
               );
@@ -134,10 +148,12 @@ const Content: FC<{
   );
 };
 
-const DatePicker: FC = () => {
+const DatePicker: FC<{
+  onChange: OnChangeType;
+}> = ({ onChange }) => {
   return (
     <>
-      <HoverPanel overlay={<Content date={new Date()} />}>
+      <HoverPanel overlay={<Content date={new Date()} onChange={onChange} />}>
         <div style={{ width: "200px" }}>
           <Input />
         </div>
