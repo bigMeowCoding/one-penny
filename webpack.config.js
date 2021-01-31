81d396a6546093ff20d0423a96a432626d0c3d23
zhouyijun <zhou495248579@hotmail.com>
2021-01-31 16:16:34 +0800

    reset umd

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist/lib"),
     library: "zyjUI",
-    libraryTarget: "commonjs",
+    libraryTarget: "umd",
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".jsx"],
