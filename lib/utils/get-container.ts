29388338e68011ba11b3f28b97f68cc4b4aaf796
zhouyijun <zhou495248579@hotmail.com>
2022-06-04 22:29:12 +0800

    refactor:增加dom 公共方法

---
+++ lib/utils/get-container.ts
@@ -0,0 +1,7 @@
+import { GetContainer } from './render-to-container'
+import isFunction from 'lodash/isFunction'
+
+export function resolveContainer(getContainer: GetContainer) {
+  const container = isFunction(getContainer) ? getContainer() : getContainer
+  return container || document.body
+}
