f648c7d6992023063fa08f5978454ce59e17c709
zhouyijun <zhou495248579@hotmail.com>
2022-06-09 22:57:48 +0800

    refactor:mergeProps 再写一遍

--- src/utils/with-default-props.tsx
+++ src/utils/with-default-props.tsx
@@ -1,17 +1,14 @@
-import assign from 'lodash/assign'
-import assignWith from 'lodash/assignWith'
-import isUndefined from 'lodash/isUndefined'
-
-export function mergeProps<A, B>(a: A, b: B): B & A
-export function mergeProps<A, B, C>(a: A, b: B, c: C): C & B & A
+import _ from 'lodash';
+export function mergeProps<A, B>(a: A, b: B): A & B;
+export function mergeProps<A, B, C>(a: A, b: B, c: C): A & B & C;
 export function mergeProps(...items: any[]) {
   function customizer(objValue: any, srcValue: any) {
-    return isUndefined(srcValue) ? objValue : srcValue
+    return _.isUndefined(objValue) ? srcValue : objValue;
   }
+  const ret = _.assign({}, items[0]);
 
-  let ret = assign({}, items[0])
   for (let i = 1; i < items.length; i++) {
-    ret = assignWith(ret, items[i], customizer)
+    _.assignWith(ret, items[i], customizer);
   }
-  return ret
+  return ret;
 }
