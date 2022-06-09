3a592605e1abb9e9a1a23ff08612d6ca465307c0
zhouyijun <zhou495248579@hotmail.com>
2022-06-09 23:42:40 +0800

    refactor:props工具 lodash树摇

--- src/utils/with-default-props.tsx
+++ src/utils/with-default-props.tsx
@@ -1,14 +1,16 @@
-import _ from 'lodash';
+import assign from 'lodash/assign';
+import assignWith from 'lodash/assignWith';
+import isUndefined from 'lodash/isUndefined';
 export function mergeProps<A, B>(a: A, b: B): A & B;
 export function mergeProps<A, B, C>(a: A, b: B, c: C): A & B & C;
 export function mergeProps(...items: any[]) {
   function customizer(objValue: any, srcValue: any) {
-    return _.isUndefined(objValue) ? srcValue : objValue;
+    return isUndefined(srcValue) ? objValue : srcValue;
   }
-  const ret = _.assign({}, items[0]);
+  const ret = assign({}, items[0]);
 
   for (let i = 1; i < items.length; i++) {
-    _.assignWith(ret, items[i], customizer);
+    assignWith(ret, items[i], customizer);
   }
   return ret;
 }
