85701f491198378c70fb76477efb5d69c77a4cd0
zhouyijun <zhou495248579@hotmail.com>
2022-06-22 00:09:51 +0800

    chore:eslint 准许ts-ignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/no-use-before-define': 'off',
     '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/camelcase': 'off',
     'react/prop-types': 'off',
     'react/jsx-no-target-blank': 'off',
@@ -79,4 +80,4 @@ module.exports = {
       },
     },
   ],
-};
+}
